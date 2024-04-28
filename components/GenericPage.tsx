a83e38f0dcc1511ce4a1f43683805b43f4d9e157
Javor Emir <emir.j2002@gmail.com>
2024-04-28 15:12:33 +0200

    [ADD] genericpage first div

--- components/GenericPage.tsx
+++ components/GenericPage.tsx
@@ -1,19 +1,22 @@
 import React from "react";
-import { Montserrat } from 'next/font/google';
+import { Montserrat } from "next/font/google";
 
 const montserrat = Montserrat({
-    subsets: ['latin'],
-    variable: '--font-montserrat',
-  });
-
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
 
 const GenericPage = () => {
   return (
     <div className={`${montserrat.variable} font-sans w-full h-screen`}>
-      <div className="bg-photo1 w-full h-[54%]  bg-no-repeat">
-        <div><h1 className="text-white text-2xl font-bold">Titre</h1></div>
+      <div className="bg-photo1 w-full h-[54%]  bg-no-repeat flex flex-col justify-end">
+        <div className="w-2/3 mx-auto pb-[3%] pl-[4%]">
+          <h1 className="text-white text-4xl font-bold ">Titre</h1>
+        </div>
       </div>
-      <div><h1>ABC</h1></div>
+      <div>
+        <h1>ABC</h1>
+      </div>
     </div>
   );
 };
