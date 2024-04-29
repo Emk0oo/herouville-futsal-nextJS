9685ff1d35a614758cf0fae322ebaa19c56be22a
Javor Emir <emir.j2002@gmail.com>
2024-04-29 20:56:53 +0200

    [UPD] footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,34 +13,53 @@ const montserrat = Montserrat({
 function Footer() {
   return (
     <footer
-      className={
-        " ${montserrat.variable} font-sans  flex flex-col text-bleuNuitHerouville shadow "
-      }
+      className={`${montserrat.variable} font-sans  text-bleuNuitHerouville shadow `}
     >
-      <div className="flex flex-col justify-center items-center  ">
-        <div>
+      <div
+        id="content"
+        className="flex justify-center items-center gap-10  border-t border-b py-[10%] "
+      >
+        <div id="image" className="">
           <Image
             src={logo}
             alt="Logo du club de futsal de Herouville"
-            width={100}
-            height={100}
+            width={170}
+            height={170}
           />
         </div>
-
-        <div className="flex flex-col gap-2 items-center">
-          <h1 className=" font-bold ">Coordonnées du club</h1>
-          <div className="flex flex-row gap-2 ">
-            {/* <Image src={pointeur} alt="icone pointeur" width={25} height={16} /> */}
-            <p className="max-w-[50%]">21 Rue du Club 1400 Herouvile</p>
+        <div id="contenuLien" className="flex gap-10">
+          <div id="coordonnees" className="flex flex-col gap-2">
+            <h1 className=" font-bold ">Coordonnées du club</h1>
+            <div className="flex flex-row gap-2 ">
+              <Image
+                src={pointeur}
+                alt="icone pointeur"
+                width={25}
+                height={16}
+              />
+              <p className="">21 Rue du Club 1400 Herouvile</p>
+            </div>
+            <div className="flex flex-row gap-2">
+              <Image src={phone} alt="icone pointeur" width={25} height={16} />
+              <p>02 31 10 20 30</p>
+            </div>
           </div>
-          <div className="flex flex-row gap-2">
-            {/* <Image src={phone} alt="icone pointeur" width={25} height={16} /> */}
-            <p>02 31 10 20 30</p>
+          <div id="liens" className="flex flex-col gap-2 ">
+            <h1 className="font-bold">Liens</h1>
+            <a href="/">Accueil</a>
+            <a href="/">Actualités</a>
+            <a href="/">Calendrier</a>
+            <a href="/">Boutique</a>
+          </div>
+          <div id="reseaux" className="flex flex-col gap-2 items-center">
+            <h1 className="font-bold">Suivez nous !</h1>
+            <a href="/">Facebook</a>
+            <a href="/">Instagram</a>
+            <a href="/">Twitter</a>
           </div>
         </div>
       </div>
-
-      <div className="flex flex-row justify-center p-4 gap-2 border-t mt-4 w-full">
+      <div className="flex flex-row justify-center p-4 gap-2  mt-4 w-full text-sm font-bold">
         <a href="/">Mentions légales</a>
         <a href="/">Plan du site</a>
       </div>
