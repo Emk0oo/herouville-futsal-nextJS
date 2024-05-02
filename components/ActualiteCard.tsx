d08b64eb18951fa9f5c20ebd7e9060ec5830458b
Javor Emir <emir.j2002@gmail.com>
2024-05-02 18:51:11 +0200

    [FIX] apostrophe

--- components/ActualiteCard.tsx
+++ components/ActualiteCard.tsx
@@ -7,7 +7,7 @@ function Card({ imageUrl, title, date, link }) {
       <div className="p-4">
         <p className="text-sm text-gray-600">{title}</p>
         <p className="text-xs text-gray-500">{date}</p>
-        <a href={link} className="mt-4 inline-block bg-blue-500 text-white rounded-full px-4 py-2">Lire l'article</a>
+        <a href={link} className="mt-4 inline-block bg-blue-500 text-white rounded-full px-4 py-2">Lire l&apos;article</a>
       </div>
     </div>
   );
