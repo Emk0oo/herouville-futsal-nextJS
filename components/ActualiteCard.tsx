3c46bc373a6c0d2fe8d0cacbbba91aea32dcafc4
Javor Emir <emir.j2002@gmail.com>
2024-05-02 18:07:27 +0200

    [ADD] actualités cards, component

---
+++ components/ActualiteCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Card({ imageUrl, title, date, link }) {
+  return (
+    <div className="bg-white rounded-lg shadow flex flex-col items-center text-center">
+      <img src={imageUrl} alt={title} className="rounded-t-lg w-full h-48 object-cover" />
+      <div className="p-4">
+        <p className="text-sm text-gray-600">{title}</p>
+        <p className="text-xs text-gray-500">{date}</p>
+        <a href={link} className="mt-4 inline-block bg-blue-500 text-white rounded-full px-4 py-2">Lire l'article</a>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- components/GenericPage.tsx
+++ components/GenericPage.tsx
@@ -6,16 +6,16 @@ const montserrat = Montserrat({
   variable: "--font-montserrat",
 });
 
-const GenericPage = () => {
+const GenericPage = ({ children, title }: { children: React.ReactNode, title: string }) => {
   return (
     <div className={`${montserrat.variable} font-sans w-full h-screen`}>
       <div className="bg-photo1 w-full h-[54%]  bg-no-repeat flex flex-col justify-end">
         <div className="w-2/3 mx-auto pb-[3%] pl-[4%]">
-          <h1 className="text-white text-4xl font-bold ">Titre</h1>
+          <h1 className="text-white text-4xl font-bold ">{title}</h1>
         </div>
       </div>
       <div>
-        <h1>ABC</h1>
+        {children} {/* Ici, les enfants passés au composant seront rendus */}{" "}
       </div>
     </div>
   );
