708e164b0ee583ac7d0b8e6ec70bacdc0526d031
Javor Emir <emir.j2002@gmail.com>
2024-05-06 14:57:58 +0200

    [ADD] init page login

---
+++ app/login/layout.tsx
@@ -0,0 +1,24 @@
+// dashboard/layout.tsx
+import React from "react";
+import "../globals.css";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
+
+export default function LoginLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fr">
+      <link rel="icon" href="/favicon.ico" />
+      <body className={`${montserrat.variable} font-sans  `}>
+        {children}
+      </body>
+    </html>
+  );
+}
