272c5b5f747e782767e47b7216401e08c8b58845
Javor Emir <emir.j2002@gmail.com>
2024-05-15 17:45:39 +0200

    responsive

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,16 +5,16 @@ import { Montserrat } from "next/font/google";
 import centerLogo1 from "@/public/div1.png";
 import centerLogo2 from "@/public/logo2.png";
 import logo3 from "@/public/logo3.png";
-
 import actu1 from "@/public/actu1.png";
 import griffe from "@/public/griffe.png";
+
+import ArticleCard from "@/components/ArticleHomeCard";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { FreeMode, Pagination } from "swiper/modules";
-
 import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/pagination";
-import ButtonLink from "@/components/ButtonLink";
+import ButtonNavigation from "@/components/ButtonNavigation";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -51,104 +51,55 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div id="Actu" className="w-full h-screen flex justify-center ">
+      <div id="Actu" className="w-full h-screen flex justify-center">
         <div
           id="actuContent"
-          className="bg-actu bg-no-repeat  w-[75%] h-2/3 rounded-2xl mt-[-5%] z-0"
+          className="bg-actu bg-no-repeat w-[90%] md:w-[75%] h-2/3 rounded-2xl mt-[-5%] z-0"
         >
           <div className="mt-[3%]">
-            <div className="flex flex-col justify-center items-center ">
-              <h1 className="text-5xl text-bleuNuitHerouville font-light">
-                Nos{" "}
+            <div className="flex flex-col justify-center items-center">
+              <h1 className="text-3xl md:text-5xl text-bleuNuitHerouville font-light">
+                Nos
               </h1>
-              <h1 className="text-5xl font-bold font uppercase text-bleuNuitHerouville">
+              <h1 className="text-3xl md:text-5xl font-bold uppercase text-bleuNuitHerouville">
                 actualités
               </h1>
             </div>
 
             <div
               id="lesActu"
-              className="flex flex-row h-[70%] justify-center gap-6 mt-[5%] "
+              className="flex flex-col md:flex-row h-[70%] justify-center gap-6 mt-[5%]"
             >
-              <div
-                id="actu1"
-                className="flex flex-col w-1/4 h-1/2 bg-white rounded-2xl shadow-md"
-              >
-                <Image
-                  src={actu1}
-                  alt="Logo du club de futsal de Herouville"
-                  className="mx-auto rounded-t-2xl h-auto object-cover"
-                />
-                <div className="flex flex-col justify-center items-center p-6 text-center">
-                  <h1 className="text-2xl mx-auto font-bold">
-                    Lorem ipsum dolor sit amet consectetur
-                  </h1>
-                  <p>Publié le 12 avril 2024</p>
-                  <ButtonLink text="Lire l'article" href="/actualites" />
-                </div>
-              </div>
-              <div
-                id="actu2"
-                className="flex flex-col w-1/4 h-1/2 bg-white rounded-2xl shadow-md mt-[3%]"
-              >
-                <Image
-                  src={actu1}
-                  alt="Logo du club de futsal de Herouville"
-                  className="mx-auto rounded-t-2xl h-auto object-cover"
-                />
-                <div className="flex flex-col justify-center items-center p-6 text-center">
-                  <h1 className="text-2xl mx-auto font-bold">
-                    Lorem ipsum dolor sit amet consectetur
-                  </h1>
-                  <p>Publié le 12 avril 2024</p>
-                  <button className="border-solid border-2 border-bleuHerouville text-bleuNuitHerouville font-bold mt- py-2 px-5 rounded-full mt-[5%]">
-                    Lire l article
-                  </button>
-                </div>
-              </div>
-              <div
-                id="actu3"
-                className="flex flex-col w-1/4 h-1/2 bg-white rounded-2xl shadow-md"
-              >
-                <Image
-                  src={actu1}
-                  alt="Logo du club de futsal de Herouville"
-                  className="mx-auto rounded-t-2xl h-auto object-cover"
-                />
-                <div className="flex flex-col justify-center items-center p-6 text-center">
-                  <h1 className="text-2xl mx-auto font-bold">
-                    Lorem ipsum dolor sit amet consectetur
-                  </h1>
-                  <p>Publié le 12 avril 2024</p>
-                  <button className="border-solid border-2 border-bleuHerouville text-bleuNuitHerouville font-bold mt- py-2 px-5 rounded-full mt-[5%]">
-                    Lire l article
-                  </button>
-                </div>
-              </div>
+              <ArticleCard
+                imageSrc="/path/to/actu1.jpg"
+                altText="Logo du club de futsal de Herouville"
+                title="Lorem ipsum dolor sit amet consectetur"
+                date="12 avril 2024"
+                href="/actualites"
+              />
+              <ArticleCard
+                imageSrc="/path/to/actu1.jpg"
+                altText="Logo du club de futsal de Herouville"
+                title="Lorem ipsum dolor sit amet consectetur"
+                date="12 avril 2024"
+                href="/actualites"
+                customClass="mt-[2%] md:mt-0"
+              />
+              <ArticleCard
+                imageSrc="/path/to/actu1.jpg"
+                altText="Logo du club de futsal de Herouville"
+                title="Lorem ipsum dolor sit amet consectetur"
+                date="12 avril 2024"
+                href="/actualites"
+              />
             </div>
           </div>
           <div id="boutonActualites" className="flex justify-center mt-[5%]">
-            <a
-              className="flex flex-row items-center gap-2 border-solid border-2 bg-bleuHerouville text-white font-bold mt- py-2 px-5 rounded-full"
-              href="/actualites"
-            >
-              Voir les actualités
-              <svg
-                width="22"
-                height="12"
-                viewBox="0 0 22 12"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M1 5.25C0.585786 5.25 0.25 5.58579 0.25 6C0.25 6.41421 0.585786 6.75 1 6.75V5.25ZM21.5303 6.53033C21.8232 6.23744 21.8232 5.76256 21.5303 5.46967L16.7574 0.696699C16.4645 0.403806 15.9896 0.403806 15.6967 0.696699C15.4038 0.989593 15.4038 1.46447 15.6967 1.75736L19.9393 6L15.6967 10.2426C15.4038 10.5355 15.4038 11.0104 15.6967 11.3033C15.9896 11.5962 16.4645 11.5962 16.7574 11.3033L21.5303 6.53033ZM1 6.75H21V5.25H1V6.75Z"
-                  fill="white"
-                />
-              </svg>
-            </a>
+            <ButtonNavigation text="Voir les actualités" href="/actualites" />
           </div>
         </div>
       </div>
+
       <div id="NotreHistoire" className="relative w-full h-screen p-4 md:p-0">
         <div className="bg-histoire bg-no-repeat w-full h-full relative flex flex-wrap">
           <div
@@ -207,51 +158,40 @@ export default function Home() {
         </div>
       </div>
       <div id="Calendrier" className="w-full h-screen mt-3 p-4 md:p-0">
-  <div id="titreCalendrier" className="flex flex-col justify-center items-center">
-    <h1 className="text-4xl md:text-5xl text-bleuNuitHerouville font-light">
-      Les prochains
-    </h1>
-    <h1 className="text-4xl md:text-5xl font-bold uppercase text-bleuNuitHerouville">
-      matchs
-    </h1>
-  </div>
+        <div
+          id="titreCalendrier"
+          className="flex flex-col justify-center items-center"
+        >
+          <h1 className="text-4xl md:text-5xl text-bleuNuitHerouville font-light">
+            Les prochains
+          </h1>
+          <h1 className="text-4xl md:text-5xl font-bold uppercase text-bleuNuitHerouville">
+            matchs
+          </h1>
+        </div>
 
-  <div id="calendrierContent" className="flex flex-col md:flex-row justify-center gap-6 mt-6 w-full h-auto md:h-1/3">
-    <div
-      id="cardCalendrier"
-      className="bg-calendrierCard bg-no-repeat w-full md:w-1/4 h-60 md:h-full bg-cover rounded-2xl shadow-md"
-    >
-      <div className="flex flex-col justify-center items-center p-6 text-center"></div>
-    </div>
-    <div
-      id="cardCalendrier"
-      className="bg-calendrierCard bg-no-repeat w-full md:w-1/4 h-60 md:h-full bg-cover rounded-2xl shadow-md"
-    >
-      <div className="flex flex-col justify-center items-center p-6 text-center"></div>
-    </div>
-  </div>
-  
-  <div id="boutonActualites" className="flex justify-center mt-6">
-    <a
-      className="flex flex-row items-center gap-2 border-solid border-2 bg-bleuHerouville text-white font-bold py-2 px-5 rounded-full"
-      href="/calendrier"
-    >
-      Voir le calendrier
-      <svg
-        width="22"
-        height="12"
-        viewBox="0 0 22 12"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M1 5.25C0.585786 5.25 0.25 5.58579 0.25 6C0.25 6.41421 0.585786 6.75 1 6.75V5.25ZM21.5303 6.53033C21.8232 6.23744 21.8232 5.76256 21.5303 5.46967L16.7574 0.696699C16.4645 0.403806 15.9896 0.403806 15.6967 0.696699C15.4038 0.989593 15.4038 1.46447 15.6967 1.75736L19.9393 6L15.6967 10.2426C15.4038 10.5355 15.4038 11.0104 15.6967 11.3033C15.9896 11.5962 16.4645 11.5962 16.7574 11.3033L21.5303 6.53033ZM1 6.75H21V5.25H1V6.75Z"
-          fill="white"
-        />
-      </svg>
-    </a>
-  </div>
-</div>
+        <div
+          id="calendrierContent"
+          className="flex flex-col md:flex-row justify-center gap-6 mt-6 w-full h-auto md:h-1/3"
+        >
+          <div
+            id="cardCalendrier"
+            className="bg-calendrierCard bg-no-repeat w-full md:w-1/4 h-60 md:h-full bg-cover rounded-2xl shadow-md"
+          >
+            <div className="flex flex-col justify-center items-center p-6 text-center"></div>
+          </div>
+          <div
+            id="cardCalendrier"
+            className="bg-calendrierCard bg-no-repeat w-full md:w-1/4 h-60 md:h-full bg-cover rounded-2xl shadow-md"
+          >
+            <div className="flex flex-col justify-center items-center p-6 text-center"></div>
+          </div>
+        </div>
+
+        <div id="boutonActualites" className="flex justify-center mt-6">
+          <ButtonNavigation text="Voir le calendrier" href="/calendrier" />
+        </div>
+      </div>
 
       <div id="newsletter" className="w-full relative p-4">
         <div
---
+++ components/ArticleHomeCard.tsx
@@ -0,0 +1,36 @@
+// components/ArticleCard.tsx
+import React from 'react';
+import Image from 'next/image';
+import ButtonLink from './ButtonLink';
+
+interface ArticleCardProps {
+  imageSrc: string;
+  altText: string;
+  title: string;
+  date: string;
+  href: string;
+  customClass?: string;
+}
+
+const ArticleCard: React.FC<ArticleCardProps> = ({ imageSrc, altText, title, date, href, customClass }) => {
+  return (
+    <div className={`flex flex-col w-full md:w-1/4 h-auto md:h-1/2 bg-white rounded-2xl shadow-md ${customClass}`}>
+      <Image
+        src={imageSrc}
+        alt={altText}
+        className="mx-auto rounded-t-2xl h-auto object-cover"
+        width={500}
+        height={300}
+      />
+      <div className="flex flex-col justify-center items-center p-6 text-center">
+        <h1 className="text-2xl mx-auto font-bold">
+          {title}
+        </h1>
+        <p>Publié le {date}</p>
+        <ButtonLink text="Lire l'article" href={href} />
+      </div>
+    </div>
+  );
+};
+
+export default ArticleCard;
