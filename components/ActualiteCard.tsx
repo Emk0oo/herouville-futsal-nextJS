c145aaa45e7571412821c4532b962969f7ac1101
Javor Emir <emir.j2002@gmail.com>
2024-05-18 21:40:55 +0200

    [ADD] use compnent

--- components/ActualiteCard.tsx
+++ components/ActualiteCard.tsx
@@ -1,6 +1,7 @@
 // ActualiteCard.tsx
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
+import ButtonLink from "./ButtonLink";
 
 interface CardProps {
   imageUrl: string;
@@ -13,19 +14,20 @@ function Card({ imageUrl, title, date, link }: CardProps) {
   return (
     <div className="bg-white rounded-lg shadow flex flex-col items-center text-center">
       <div className="relative w-full h-48 rounded-t-lg">
-        <Image 
-          src={imageUrl} 
-          alt={title} 
-          layout="fill" 
-          objectFit="cover" 
+        <Image
+          src={imageUrl}
+          alt={title}
+          layout="fill"
+          objectFit="cover"
           className="rounded-t-lg"
         />
       </div>
       <div className="p-4">
         <p className="text-sm text-gray-600">{title}</p>
         <p className="text-xs text-gray-500">{date}</p>
-      
-        <a href={link} className="mt-4 inline-block bg-blue-500 text-white rounded-full px-4 py-2">Lire l&apos;article</a> 
+        <div className="mt-5">
+          <ButtonLink text="Lire l'article" href={link} className="p-6" />
+        </div>
       </div>
     </div>
   );
