1533f4af63de3b710eb61f92ea2d0d06337ba5c8
Javor Emir <emir.j2002@gmail.com>
2024-05-18 18:36:53 +0200

    [ADD] Article mobile

--- app/(root)/actualites/[id]/page.tsx
+++ app/(root)/actualites/[id]/page.tsx
@@ -1,23 +1,27 @@
-// app/(root)/actualites/[id]/page.tsx
-
 "use client";
 
 import React from "react";
 import { usePathname } from "next/navigation";
 import Image from "next/image";
+import imgArticle from "@/public/calendrierCard.png";
 
 const articles = [
   {
     id: 1,
-    imageUrl: "/path_to_image1.jpg",
-    title: "Lorem ipsum dolor sit amet consectetur",
+    imageUrl: imgArticle,
+    titre: "Lorem ipsum dolor sit amet consectetur",
+    titre2: "Lorem ipsum dolor sit amet ",
+    titre3: "Tincidunt ornare massa eget ",
     date: "Publié le: 12 avril 2023",
-    content: "Contenu de l'article 1...",
-  },
+    entete:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est doloribus a eveniet, et minus fugiat repellendus laudantium voluptatum nostrum laboriosam, molestiae incidunt hic harum maxime? Voluptatibus qui unde ad? Delectus?",
+    content:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi inventore adipisci magni at corrupti recusandae blanditiis. Reiciendis, hic. Quis, dolore vel. Recusandae beatae quisquam cupiditate, fugiat voluptatem dolore, dolorum exercitationem nemo quasi totam libero sequi magni deleniti? Recusandae sequi quisquam repellat itaque facere minima incidunt illum ea, ad alias pariatur voluptate magni doloribus quos dolores omnis illo sunt repudiandae voluptatem quasi, dolorem aliquid voluptas optio? Qui consectetur iure earum soluta quibusdam odio, vero iste aliquid cum, deserunt, maiores accusantium incidunt quaerat. Vel id illo delectus aperiam commodi.",
+     },
   {
     id: 2,
-    imageUrl: "/path_to_image2.jpg",
-    title: "Lorem ipsum dolor sit amet consectetur",
+    imageUrl: imgArticle,
+    titre: "Lorem ipsum dolor sit amet consectetur",
     date: "Publié le: 10 avril 2023",
     content: "Contenu de l'article 2...",
   },
@@ -35,17 +39,29 @@ const ActualiteDetail = () => {
 
   return (
     <div className="flex justify-center">
-      <div id="contentArticle" className="w-full md:w-1/2 p-4">
-        <Image
-          src={article.imageUrl}
-          alt={article.title}
-          layout="fill"
-          objectFit="cover"
-          className="rounded-t-lg"
-        />
-        <h1 className="text-2xl font-bold mt-4">{article.title}</h1>
-        <p className="text-sm text-gray-500">{article.date}</p>
-        <p className="mt-4">{article.content}</p>
+      {/* Ici component article */}
+      <div id="Article" className="w-full md:w-1/2 p-4">
+        <div id="headerArticle" className="flex flex-col ">
+          <Image
+            src={article.imageUrl}
+            alt={article.titre}
+            layout="responsive"
+            width={700}
+            height={475}
+            objectFit="cover"
+            className=""
+          />
+          <h1 className="text-3xl font-bold mt-4 text-bleuNuitHerouville">
+            {article.titre}
+          </h1>
+          <p className="text-xs text-gray-500 mt-1">{article.date}</p>
+        </div>
+        <div id="contentArticle" className="flex flex-col mt-4">
+          <p className="mt-4 text-bleuNuitHerouville font-bold">{article.entete}</p>
+          <p className="mt-4 text-xl font-bold text-bleuNuitHerouville underline decoration-jauneHerouville decoration-4">{article.titre2}</p>
+          <p className="mt-4 font-bold bg-bleuHerouville text-white inline-block">{article.titre3}</p>
+          <p className="mt-4">{article.content}</p>
+        </div>
       </div>
     </div>
   );
