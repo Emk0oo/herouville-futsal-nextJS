b796d532caa582fcc3c9c16523ea87d4a5065b0e
Javor Emir <emir.j2002@gmail.com>
2024-05-23 19:34:04 +0200

    [FIX] meta calendrier

--- app/(backoffice)/dashboard/calendrier/page.tsx
+++ app/(backoffice)/dashboard/calendrier/page.tsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import {Metadata} from 'next'
+
+export const metadata: Metadata = {
+  title: 'Herouville Futsal - Dashboard - Calendrier',
+  description: 'Calendrier des événements.'
+}
 
 export default function Calendrier() {
   return (
