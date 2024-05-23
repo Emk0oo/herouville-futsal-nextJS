f0ec72718497e7788359195eefe941f20c1fa0c9
Javor Emir <emir.j2002@gmail.com>
2024-05-23 16:01:00 +0200

    [ADD] hooks token checking

--- app/(backoffice)/dashboard/page.tsx
+++ app/(backoffice)/dashboard/page.tsx
@@ -1,9 +1,10 @@
 // dashboard/page.tsx
-import Dashboard from "@/components/Dashboard";
-import DashboardContainer from "@/components/DashboardContainer";
+"use client"
+import useAuth from "@/hooks/useAuth";
 
 export default function DashboardPage() {
   return (
+    useAuth(),
     <>
    
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem veritatis quos dolorum nobis praesentium fuga natus repudiandae autem, vero tempore iste veniam in ea eius dolore accusantium nam reprehenderit sapiente.
