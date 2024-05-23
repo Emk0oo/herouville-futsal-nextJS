22f049011d60c6c83813280a669896608682af03
Javor Emir <emir.j2002@gmail.com>
2024-05-23 19:35:14 +0200

    [FIX] meta boutique dashbaord

--- app/(backoffice)/dashboard/boutique/page.tsx
+++ app/(backoffice)/dashboard/boutique/page.tsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import {Metadata} from 'next'
+
+export const metadata: Metadata = {
+  title: 'Herouville Futsal - Dashboard - Boutique',
+  description: 'Gestion de la boutique.'
+}
 
 export default function Boutique() {
   return (
