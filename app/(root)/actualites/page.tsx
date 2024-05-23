2f9f345aa16ad7f9e5d3b36a38b6f6134514bac7
Javor Emir <emir.j2002@gmail.com>
2024-05-23 18:51:10 +0200

    [FIX] metadata title component actualite

--- app/(root)/actualites/page.tsx
+++ app/(root)/actualites/page.tsx
@@ -1,91 +1,16 @@
-"use client";
-import ActualiteCard from "@/components/ActualiteCard";
-import Pagination from "@/components/Pagination";
-import Head from "next/head";
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { Metadata } from "next";
+import ActualiteHome from "@/components/homepage/ActualiteHome";
 
-const CARDS_PER_PAGE = 12;
+export const metadata: Metadata = {
+  title: "Herouville Futsal - Actualite",
+  description: "Consultez les dernières actualités de notre site.",
+};
 
-export default function Actualites() {
-  const [currentPage, setCurrentPage] = useState<number>(1);
-  const [cardsData, setCardsData] = useState<any[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchArticles = async () => {
-      try {
-        const res = await fetch("http://localhost:4000/article");
-        if (!res.ok) {
-          throw new Error("Failed to fetch articles");
-        }
-        const data = await res.json();
-        setCardsData(data);
-      } catch (err) {
-        if (err instanceof Error) {
-          setError(err.message);
-        } else {
-          setError("An unknown error occurred");
-        }
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchArticles();
-  }, []);
-
-  const totalPages = Math.ceil(cardsData.length / CARDS_PER_PAGE);
-
-  const getPaginatedData = () => {
-    const startIndex = (currentPage - 1) * CARDS_PER_PAGE;
-    const endIndex = startIndex + CARDS_PER_PAGE;
-    return cardsData.slice(startIndex, endIndex);
-  };
-
-  const handlePageChange = (pageNumber: number) => {
-    setCurrentPage(pageNumber);
-  };
-
+export default function Actualite() {
   return (
     <>
-      <Head>
-        <title>Actualités - Mon Site</title>
-        <meta
-          name="description"
-          content="Consultez les dernières actualités de notre site."
-        />
-        <meta
-          name="keywords"
-          content="actualités, news, articles, informations"
-        />
-      </Head>
-      <div className="flex justify-center w-full">
-        {isLoading ? (
-          <p>Loading...</p>
-        ) : error ? (
-          <p>Error: {error}</p>
-        ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4 w-2/3 mx-[15%] ">
-            {getPaginatedData().map((card, index) => (
-              <ActualiteCard
-                key={index}
-                imageUrl={card.imageURL}
-                title={card.title}
-                date={card.date}
-                link={`/actualites/${card.id}`}
-              />
-            ))}
-          </div>
-        )}
-      </div>
-      {!isLoading && !error && (
-        <Pagination
-          currentPage={currentPage}
-          totalPages={totalPages}
-          onPageChange={handlePageChange}
-        />
-      )}
+    <ActualiteHome />
     </>
   );
 }
