4821dff852f2613a75f0c4a8f122370169b2c241
Javor Emir <emir.j2002@gmail.com>
2024-05-23 19:33:05 +0200

    [FIX] meta data title equipe dashboard

--- app/(backoffice)/dashboard/equipe/page.tsx
+++ app/(backoffice)/dashboard/equipe/page.tsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import {Metadata} from 'next'
+
+export const metadata: Metadata = {
+  title: 'Herouville Futsal - Dashboard - Equipe',
+  description: 'Page de l\'équipe du site.',
+};
 
 export default function Equipe() {
   return (
