1d0edea29bf8ec485eb257c0a34e5f475f8f0c4b
Javor Emir <emir.j2002@gmail.com>
2024-05-26 22:04:09 +0200

    [ADD] handle mutler product route

--- api/controllers/product.controller.js
+++ api/controllers/product.controller.js
@@ -1,16 +1,19 @@
 const connection = require('../config/database');
 
+// Créer un nouveau produit
 exports.createProduct = (req, res) => {
   const { name, description, price, stock } = req.body;
-  const query = 'INSERT INTO produits (name, description, price, stock) VALUES (?, ?, ?, ?)';
-  connection.query(query, [name, description, price, stock], (err, results) => {
+  const imageURL = req.file ? `/${req.file.originalname}` : null; // Chemin de l'image téléchargée
+  const query = 'INSERT INTO produits (name, description, price, stock, imageURL) VALUES (?, ?, ?, ?, ?)';
+  connection.query(query, [name, description, price, stock, imageURL], (err, results) => {
     if (err) {
       return res.status(500).json({ error: err.message });
     }
-    res.status(201).json({ id: results.insertId, name, description, price, stock });
+    res.status(201).json({ id: results.insertId, name, description, price, stock, imageURL });
   });
 };
 
+// Obtenir tous les produits
 exports.getProducts = (req, res) => {
   const query = 'SELECT * FROM produits';
   connection.query(query, (err, results) => {
@@ -21,6 +24,7 @@ exports.getProducts = (req, res) => {
   });
 };
 
+// Obtenir un produit par ID
 exports.getProductById = (req, res) => {
   const { id } = req.params;
   const query = 'SELECT * FROM produits WHERE id = ?';
@@ -35,21 +39,44 @@ exports.getProductById = (req, res) => {
   });
 };
 
+// Mettre à jour un produit par ID
 exports.updateProduct = (req, res) => {
   const { id } = req.params;
   const { name, description, price, stock } = req.body;
-  const query = 'UPDATE produits SET name = ?, description = ?, price = ?, stock = ? WHERE id = ?';
-  connection.query(query, [name, description, price, stock, id], (err, results) => {
+
+  // Fetch the current image URL from the database
+  const getCurrentImageURLQuery = 'SELECT imageURL FROM produits WHERE id = ?';
+  connection.query(getCurrentImageURLQuery, [id], (err, results) => {
     if (err) {
       return res.status(500).json({ error: err.message });
     }
-    if (results.affectedRows === 0) {
+    if (results.length === 0) {
       return res.status(404).json({ message: 'Product not found' });
     }
-    res.status(200).json({ message: 'Product updated successfully' });
+
+    const currentImageURL = results[0].imageURL;
+
+    let imageURL = currentImageURL; // Default to current image URL
+    if (req.file) {
+      imageURL = `/${req.file.originalname}`; // Use new image URL if provided
+    }
+
+    const query = 'UPDATE produits SET name = ?, description = ?, price = ?, stock = ?, imageURL = ? WHERE id = ?';
+    const params = [name, description, price, stock, imageURL, id];
+
+    connection.query(query, params, (err, results) => {
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
+      if (results.affectedRows === 0) {
+        return res.status(404).json({ message: 'Product not found' });
+      }
+      res.status(200).json({ id, name, description, price, stock, imageURL });
+    });
   });
 };
 
+// Supprimer un produit par ID
 exports.deleteProduct = (req, res) => {
   const { id } = req.params;
   const query = 'DELETE FROM produits WHERE id = ?';
@@ -60,6 +87,6 @@ exports.deleteProduct = (req, res) => {
     if (results.affectedRows === 0) {
       return res.status(404).json({ message: 'Product not found' });
     }
-    res.status(200).json({ message: 'Product deleted successfully' });
+    res.status(204).send();
   });
 };
--- api/routes/product.route.js
+++ api/routes/product.route.js
@@ -3,22 +3,22 @@ const router = express.Router();
 const multer = require('multer');
 const path = require('path');
 const productController = require('../controllers/product.controller');
-const checkToken  = require('../middleware/checkAdminToken');
+const checkToken = require('../middleware/checkAdminToken');
 
 // Configuration de Multer pour stocker les images dans le dossier public
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '../../public/'));
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname); // Conserve le nom original du fichier
-    }
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, '../../public/'));
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); // Conserve le nom original du fichier
+  }
 });
 
 const upload = multer({ storage: storage });
 
 // Créer un nouveau produit
-router.post('/', checkToken, productController.createProduct);
+router.post('/', checkToken, upload.single('image'), productController.createProduct);
 
 // Obtenir tous les produits
 router.get('/', productController.getProducts);
@@ -27,7 +27,7 @@ router.get('/', productController.getProducts);
 router.get('/:id', productController.getProductById);
 
 // Mettre à jour un produit par ID
-router.put('/:id', checkToken, productController.updateProduct);
+router.put('/:id', checkToken, upload.single('image'), productController.updateProduct);
 
 // Supprimer un produit par ID
 router.delete('/:id', checkToken, productController.deleteProduct);
