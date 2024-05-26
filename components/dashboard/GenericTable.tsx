e3296ac9bc7db82849afc8a72eefb29232693ffd
Javor Emir <emir.j2002@gmail.com>
2024-05-27 01:24:39 +0200

    update css table

--- components/dashboard/GenericTable.tsx
+++ components/dashboard/GenericTable.tsx
@@ -1,6 +1,5 @@
-// components/GenericTable.tsx
 "use client";
-import React, { ReactNode } from "react";
+import React from "react";
 import Link from "next/link";
 import { CiEdit, CiTrash } from "react-icons/ci";
 import { MdAddCircleOutline } from "react-icons/md";
@@ -22,22 +21,20 @@ const GenericTable = <T extends { id: number }>({
 }: GenericTableProps<T>) => {
   return (
     <div className="w-full">
-      <div className="container mx-auto p-4">
-        <div id="boutonNouveau" className="flex justify-end">
+      <div className="container mx-auto p-6 bg-white shadow-lg rounded-lg">
+        <div className="flex justify-end mb-4">
           <Link
             href={addUrl}
-            className=" mb-2 p-2 border rounded-xl bg-green-500"
+            className="flex items-center px-4 py-2 bg-green-500 text-white font-semibold rounded-lg shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400"
           >
-            <div className="flex items-center gap-2 text-white font-bold">
-              <p>Ajouter</p>
-              <MdAddCircleOutline size={30} />
-            </div>
+            <MdAddCircleOutline size={24} className="mr-2" />
+            Ajouter
           </Link>
         </div>
         <div className="overflow-x-auto">
-          <table className="min-w-full bg-white border rounded-lg">
+          <table className="min-w-full bg-white">
             <thead>
-              <tr className="w-full bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+              <tr className="bg-gray-100 text-gray-600 uppercase text-sm leading-normal">
                 {columns.map((column) => (
                   <th key={column.key as string} className="py-3 px-6 text-left">
                     {column.header}
@@ -46,10 +43,10 @@ const GenericTable = <T extends { id: number }>({
                 <th className="py-3 px-6 text-left">Opération</th>
               </tr>
             </thead>
-            <tbody className="text-gray-600 text-sm font-light">
+            <tbody className="text-gray-700 text-sm">
               {data.length === 0 ? (
                 <tr>
-                  <td colSpan={columns.length + 1} className="text-center py-3">
+                  <td colSpan={columns.length + 1} className="text-center py-4">
                     Aucune donnée disponible
                   </td>
                 </tr>
@@ -60,23 +57,26 @@ const GenericTable = <T extends { id: number }>({
                     className="border-b border-gray-200 hover:bg-gray-100"
                   >
                     {columns.map((column) => (
-                      <td key={column.key as string} className="py-3 px-6 text-left">
+                      <td
+                        key={column.key as string}
+                        className="py-3 px-6 text-left"
+                      >
                         {String(item[column.key])}
                       </td>
                     ))}
                     <td className="py-3 px-6 text-left">
-                      <div className="flex ">
+                      <div className="flex space-x-4">
                         <Link
                           href={`${editUrl}/${item.id}`}
-                          className="text-blue-500 hover:text-blue-700 mr-2"
+                          className="text-blue-500 hover:text-blue-700"
                         >
-                          <CiEdit size={32} />
+                          <CiEdit size={24} />
                         </Link>
                         <button
                           onClick={() => deleteHandler(item.id)}
                           className="text-red-500 hover:text-red-700"
                         >
-                          <CiTrash size={32} />
+                          <CiTrash size={24} />
                         </button>
                       </div>
                     </td>
